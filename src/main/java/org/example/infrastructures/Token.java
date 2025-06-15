8416e3275b8820683562dcc8f6c3902b09ba9339
Van de Sande Samuel <s.vandesande@student.helmo.be>
2025-06-15 14:43:05 +0200

    ajout nouvelle branche

--- src/main/java/org/example/infrastructures/Token.java
+++ src/main/java/org/example/infrastructures/Token.java
@@ -3,7 +3,7 @@ package org.example.infrastructures;
 public class Token {
     private final String TOKEN;
     public Token() {
-        TOKEN="MTI3NDM1Nzg3NzYxOTk0OTU3OA.GZ6AuE.tPYGRwo172-2HUw4ljsWWvk7Y7Dl4xEBDXun-0";
+        TOKEN="";
     }
     public String TOKEN() {
         return TOKEN;
